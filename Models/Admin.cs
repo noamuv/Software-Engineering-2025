29def15db458ba6449b915f604f9c4f194fd0560
nailamayanja <nailamayanja@outlook.com>
2025-11-02 19:20:09 +0000

    Update Admin model with correct PK/FK structure

--- Models/Admin.cs
+++ Models/Admin.cs
@@ -1,24 +1,22 @@
 using System;
-using System.ComponentModel.DataAnnoations;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Software_Engineering_2025.Models
-
 {
     public class Admin
     {
         [Key]
-        public int Admin_ID { get; set; }
-
         [ForeignKey("User")]
-        public int User_ID { get; set; } // Foreign Key
+        public int User_ID { get; set; }  // PK/FK 
 
-        public string Admin_Level { get; set; }
+        [StringLength(100)]
+        public string? Admin_Level { get; set; }  // Indicates Admin hierarchy - "Super Admin", "Admin"
 
-        public string Privelages { get; set; }
-        public string User_Type { get; set; }
         public DateTime? Last_Login { get; set; }
-        
-        public User User { get; set; }
 
+        // Navigation property
+        public virtual User? User { get; set; }  // Links back to User table
     }
+}
 }
