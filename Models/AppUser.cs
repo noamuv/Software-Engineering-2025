a13e5dad21681ab779383cbd251cffdd96bebfba
Wuraola Salawu <salawuraola23@gmail.com>
2025-11-29 11:47:11 +0000

    Working with sensore data

--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -1,16 +1,30 @@
+// <summary>
+// AppUser.cs
+// Defines the AppUser model representing users in the application.
+
 using System;
+
 
 namespace Software_Engineering_2025.Models
 {
     public class AppUser
     {
         public Guid Id { get; set; }
-
+        
+        // User's email address, used for login
         public required string Email { get; set; }
 
-        
-        public required string? FirstName { get; set; } 
-        public required string? LastName { get; set; } 
+       // Admin adds first and last name for user profile
+        public string? FirstName { get; set; } 
+
+        public string? LastName { get; set; } 
+
+        /* The pressure data given by Sensore 
+          is titled with a unique CSV user ID and dates for user identification.
+            This ID is linked to the AppUser for data association
+            */
+        public string? CsvUserId { get; set; }
+    
 
         //Admin sends a temporary password for first login
         public string TemporaryPassword { get; set; } = null;
